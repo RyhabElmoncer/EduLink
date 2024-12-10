886ebb779b3345d4f2fb9f8237233d890fc45bf2
ryhab <ryhabelmoncerr@gmail.com>
2024-12-10 14:04:41 -0700

    😎 ️ add envt fonction 🙂

--- src/main/java/com/EduLink/DTO/EventDTO.java
+++ src/main/java/com/EduLink/DTO/EventDTO.java
@@ -1,11 +1,14 @@
 package com.EduLink.DTO;
 
+import com.EduLink.Models.Event;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,6 +20,14 @@ public class EventDTO {
     private long dateTime;
     private UserDTO organizer;
     private List<UserDTO> participants;
+    public EventDTO(Event event) {
+        this.id = event.getId();
+        this.name = event.getName();
+        this.description = event.getDescription();
+        this.dateTime = event.getDateTime();
+        this.organizer = new UserDTO(event.getOrganizer());
+        this.participants = event.getParticipants().stream().map(UserDTO::new).collect(Collectors.toList());
+    }
 }
 
 
--- src/main/java/com/EduLink/DTO/UserDTO.java
+++ src/main/java/com/EduLink/DTO/UserDTO.java
@@ -1,5 +1,6 @@
 package com.EduLink.DTO;
 
+import com.EduLink.Models.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,9 +20,24 @@ public class UserDTO {
     private String address;          // Adresse de l'utilisateur
     private String fieldOfStudy;     // Domaine d'études
     private String role;
+
+    // Constructor accepting a User instance
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.bio = user.getBio();
+        this.profileImage = user.getProfileImage();
+        this.address = user.getAddress();
+        this.fieldOfStudy = user.getFieldOfStudy();
+    }
+
+    // Correct constructor for manual instantiation
     public UserDTO(String id, String firstName, String lastName, String email) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-    }}
+    }
+}
